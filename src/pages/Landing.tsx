6460535871937d6f9d2693652c0bb4938bf6e31f
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-05 00:53:12 +0900

    landing page style: responsive design

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,19 +1,36 @@
+import wallet from '../assets/wallet.svg';
+import logo from '../assets/logo.svg';
+import login from '../assets/login.svg';
+import { Link, Outlet } from 'react-router-dom';
+import '../landing.css';
+
 export default function Landing() {
-    return(
+  return (
     <>
-    <header>
-        <div>
-            <img src="" alt="" />
-            <h1>Cryptrack</h1>
+      <header className="header">
+        <div className="title">
+          <img className="logo" src={logo} alt="logo" />
+          <h1>Cryptrack</h1>
         </div>
-        <div>
-            
+        <div className="">
+          <Link to="/signin" className="">
+            <img src={login} alt="Sign In" className="signin-svg" />
+            <span className="signin-text">Sign In</span>
+          </Link>
         </div>
-    </header>
-    <main>
-        <img src="" alt="" />
-        <div></div>
-    </main>
+      </header>
+      <Outlet />
+      <main className="main">
+        <div className="text">
+          <span className="">Simplify</span>
+          <span className="">Your Crypto</span>
+          <span className="">Investment tracking</span>
+        </div>
+        <img className="wallet" src={wallet} alt="wallet-coin" />
+        <Link to="/register">
+          <button className="landing-btn">Get started...</button>
+        </Link>
+      </main>
     </>
-    )
+  );
 }
