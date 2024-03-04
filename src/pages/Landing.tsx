b5d92880009e47aeba766751f5db1990247bc0cb
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-04 19:36:42 +0900

    feat: landing page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,19 @@
+export default function Landing() {
+    return(
+    <>
+    <header>
+        <div>
+            <img src="" alt="" />
+            <h1>Cryptrack</h1>
+        </div>
+        <div>
+            
+        </div>
+    </header>
+    <main>
+        <img src="" alt="" />
+        <div></div>
+    </main>
+    </>
+    )
+}
