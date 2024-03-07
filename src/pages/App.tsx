3603ca1c7cfc50503503d6dbba3df42d0b1788dd
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-07 23:49:07 +0900

    feat menu button

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,16 @@ import logo from '../assets/logo.svg';
 import '../App.css';
 import OverviewCard from '../components/OverviewCard.tsx';
 import CryptoTable from '../components/CryptoTable.tsx';
+import menu from '../assets/menu.svg';
 
 export default function App() {
   return (
     <>
-      <header>
+      <header className="app-header">
         <img className="app-logo" src={logo} alt="" />
+        <button>
+          <img className="menu-btn" src={menu} alt="menu button" />
+        </button>
       </header>
       <OverviewCard />
       <CryptoTable />
