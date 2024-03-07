152d13b8045e430d067d0841e406254d9db297f8
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-07 20:56:26 +0900

    feat: create transactions table

---
+++ src/components/TransactionTable.tsx
@@ -0,0 +1,52 @@
+import './CryptoTable.css';
+import add from '../assets/btn+.svg';
+
+export default function TransactionTable() {
+  return (
+    <>
+      <div className="crypto-container">
+        <div className="crypto-table">
+          <div className="crypto-nav">
+            <span>Assets</span>
+            <button>
+              <img src={add} alt="+ button" />
+            </button>
+          </div>
+          <div className="table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Price</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>16/02/24</td>
+                  <td>$ 67543.43</td>
+                  <td>$ 100</td>
+                </tr>
+                <tr>
+                  <td>05/01/24</td>
+                  <td>$ 60345</td>
+                  <td>$ 250</td>
+                </tr>
+                <tr>
+                  <td>23/12/23</td>
+                  <td>$ 54484</td>
+                  <td>$ 100</td>
+                </tr>
+                <tr>
+                  <td>20/12/23</td>
+                  <td>$ 66783</td>
+                  <td>$ 260</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
