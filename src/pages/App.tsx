4db950f0bfcc283557973f5087867394beb68e05
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-08 18:21:48 +0900

    add dropdown to the page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,16 +2,14 @@ import logo from '../assets/logo.svg';
 import '../App.css';
 import OverviewCard from '../components/OverviewCard.tsx';
 import CryptoTable from '../components/CryptoTable.tsx';
-import menu from '../assets/menu.svg';
+import DropDown from '../components/ui/DropDown.tsx';
 
 export default function App() {
   return (
     <>
       <header className="app-header">
         <img className="app-logo" src={logo} alt="" />
-        <button>
-          <img className="menu-btn" src={menu} alt="menu button" />
-        </button>
+        <DropDown />
       </header>
       <OverviewCard />
       <CryptoTable />
