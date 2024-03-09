e25235c2e662b44ce9f30b79b13e214db10528d8
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-09 21:41:40 +0900

    feat: logic to change tables rendering

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,16 +3,40 @@ import '../App.css';
 import OverviewCard from '../components/OverviewCard.tsx';
 import CryptoTable from '../components/CryptoTable.tsx';
 import DropDown from '../components/ui/DropDown.tsx';
+import TransactionTable from '../components/TransactionTable.tsx';
+import { useState } from 'react';
 
 export default function App() {
+  const [selectedCrypto, setSelectCrypto] = useState<string | null>(null);
+  const [totalPortfolioValue, setTotalPortfolioValue] = useState<number>(0);
+
+  const handleCryptoClick = (cryptoName: string) => {
+    setSelectCrypto(cryptoName);
+  };
+  const handleBackClick = () => {
+    setSelectCrypto(null);
+  };
+
   return (
     <>
       <header className="app-header">
         <img className="app-logo" src={logo} alt="" />
         <DropDown />
       </header>
-      <OverviewCard />
-      <CryptoTable />
+
+      <OverviewCard totalPortfolioValue={totalPortfolioValue} />
+      {selectedCrypto ? (
+        <TransactionTable
+          cryptoName={selectedCrypto}
+          onBack={handleBackClick}
+        />
+      ) : (
+        <CryptoTable
+          onCryptoClick={handleCryptoClick}
+          setTotalPortfolioValue={setTotalPortfolioValue}
+        />
+      )}
+
       <footer></footer>
     </>
   );
