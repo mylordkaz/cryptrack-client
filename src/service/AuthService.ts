cf10da2ea360238ba54bd69c505560329ce5b43c
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-09 11:12:55 +0900

    feat: separate logout logic

---
+++ src/service/AuthService.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const logout = async (): Promise<boolean> => {
+  try {
+    const response = await axios.post(
+      'http://localhost:3000/auth/logout',
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+
+    if (response.status === 200) {
+      console.log('Logout successful');
+
+      return true;
+    } else {
+      console.error('Failed to logout');
+      return false;
+    }
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error('Axios error during logout:', error.message);
+    } else if (error instanceof Error) {
+      console.error('General error during logout:', error.message);
+    }
+    return false;
+  }
+};
+
+export { logout };
