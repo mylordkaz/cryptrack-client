da914cdd8535e2ef7af884f7525c38c5ed57e4fe
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-10 12:34:41 +0900

    fetch db for amount holding

---
+++ src/service/useCryptoData.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface TotalHoldingData {
+  [cryptoName: string]: number;
+}
+
+const useCryptoData = () => {
+  const [totalHoldingData, setTotalHoldingData] = useState<TotalHoldingData>(
+    {}
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:3000/transactions/total',
+          {
+            withCredentials: true,
+          }
+        );
+
+        setTotalHoldingData(response.data.totalAmounts);
+        console.log(response.data.totalAmounts);
+      } catch (error: any) {
+        // Handle errors as needed
+      }
+    };
+
+    fetchData();
+  }, [setTotalHoldingData]);
+
+  return totalHoldingData;
+};
+
+export default useCryptoData;
