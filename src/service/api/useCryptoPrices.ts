c55e58690c307f2ebf621b88f0ba259ae0af56e7
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-10 16:00:43 +0900

    feat: add cache logic to limit the fetch

--- src/service/api/useCryptoPrices.ts
+++ src/service/api/useCryptoPrices.ts
@@ -11,12 +11,31 @@ const useCryptoPrices = () => {
 
   useEffect(() => {
     const fetchCryptoPrices = async () => {
+      const cacheKey = 'cryptoPrices';
+      const cacheData = localStorage.getItem(cacheKey);
+
+      if (cacheData) {
+        const { data, timestamp } = JSON.parse(cacheData);
+        const now = new Date();
+
+        if (now.getTime() - timestamp < 5 * 60 * 1000) {
+          setCryptoPrices(data);
+          return;
+        }
+      }
       try {
         const response = await axios.get(
           'http://localhost:3000/cryptos/prices',
           {
             withCredentials: true,
           }
+        );
+        localStorage.setItem(
+          cacheKey,
+          JSON.stringify({
+            data: response.data.cryptoPrices,
+            timestamp: new Date().getTime(),
+          })
         );
 
         setCryptoPrices(response.data.cryptoPrices as CryptoData[]);
