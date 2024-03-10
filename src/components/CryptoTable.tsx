f45cb186a87fb44c8873f206733dbd58081f84f8
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-10 12:37:02 +0900

    clean up component from fetching logic

--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -1,98 +1,16 @@
 import './CryptoTable.css';
 import add from '../assets/btn+.svg';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
-interface CryptoData {
-  name: string;
-  price: number;
-}
+import useCryptoData from '../service/useCryptoData';
+import useCryptoPrices from '../service/api/useCryptoPrices';
+
 interface CryptoTableProps {
   onCryptoClick: (cryptoName: string) => void;
-  setTotalPortfolioValue: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function CryptoTable({
-  onCryptoClick,
-  setTotalPortfolioValue,
-}: CryptoTableProps) {
-  const [totalHoldingData, setTotalHoldingData] = useState<
-    Record<string, number>
-  >({});
-  const [cryptoPrices, setCryptoPrices] = useState<CryptoData[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          'http://localhost:3000/transactions/total',
-          {
-            withCredentials: true,
-          }
-        );
-
-        setTotalHoldingData(response.data.totalAmounts);
-        console.log(response.data.totalAmounts);
-      } catch (error: any) {
-        if (axios.isAxiosError(error)) {
-          const axiosError = error;
-          if (axiosError.response) {
-            console.error(
-              'Server responded with error:',
-              axiosError.response.data
-            );
-          } else if (axiosError.request) {
-            console.error(
-              'No response received from the server:',
-              axiosError.request
-            );
-          } else {
-            console.error('Error setting up the request:', axiosError.message);
-          }
-        } else {
-          // Handle non-Axios errors
-          console.error('Non-Axios error:', error.message);
-        }
-      }
-    };
-
-    const fetchCryptoPrices = async () => {
-      try {
-        const response = await axios.get(
-          'http://localhost:3000/cryptos/prices',
-          {
-            withCredentials: true,
-          }
-        );
-
-        setCryptoPrices(response.data.cryptoPrices as CryptoData[]);
-        console.log(response.data.cryptoPrices);
-      } catch (error: any) {
-        console.error('Error fetching crypto prices:', error.message);
-      }
-    };
-
-    const calculateTotalportfolioValue = () => {
-      let sum = 0;
-      Object.entries(totalHoldingData).forEach(
-        ([cryptoName, holdingAmount]) => {
-          const crypto = cryptoPrices.find(
-            ({ name }) =>
-              name.toLocaleLowerCase() === cryptoName.toLocaleLowerCase()
-          );
-          const price = crypto ? crypto.price : 0;
-          const totalValue = price * holdingAmount;
-          sum += totalValue;
-        }
-      );
-
-      setTotalPortfolioValue(sum);
-    };
-
-    fetchData();
-    fetchCryptoPrices();
-    calculateTotalportfolioValue();
-  }, []);
+export default function CryptoTable({ onCryptoClick }: CryptoTableProps) {
+  const totalHoldingData = useCryptoData();
+  const cryptoPrices = useCryptoPrices();
 
   const handleCryptoRowClick = (cryptoName: string) => {
     onCryptoClick(cryptoName);
@@ -128,6 +46,7 @@ export default function CryptoTable({
 
                     const price = crypto ? crypto.price : 0;
                     const totalValue = price * holdingAmount;
+
                     return (
                       <tr
                         key={cryptoName}
@@ -150,3 +69,16 @@ export default function CryptoTable({
     </>
   );
 }
+
+// const calculateTotalportfolioValue = () => {
+//   let sum = 0;
+//   Object.entries(totalHoldingData).forEach(([cryptoName, holdingAmount]) => {
+//     const crypto = cryptoPrices.find(
+//       ({ name }) =>
+//         name.toLocaleLowerCase() === cryptoName.toLocaleLowerCase()
+//     );
+//     const price = crypto ? crypto.price : 0;
+//     const totalValue = price * holdingAmount;
+//     sum += totalValue;
+//   });
+// };
