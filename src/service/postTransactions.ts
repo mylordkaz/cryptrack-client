21dff34817419128e18891b9d9a981d3b68c3ff2
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-11 21:03:00 +0900

    feat: logic to post new transaction

---
+++ src/service/postTransactions.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const addTransaction = async (
+  selectedCoin: string,
+  selectedCryptoPrice: number,
+  quantity: number
+) => {
+  try {
+    const response = await axios.post(
+      'http://localhost:3000/transactions/add',
+      { name: selectedCoin, price: selectedCryptoPrice, quantity: quantity },
+      { withCredentials: true }
+    );
+    console.log('Transaction added:', response.data);
+    return response.data;
+  } catch (error: any) {
+    console.error('Failed to add transaction:', error);
+    throw error;
+  }
+};
