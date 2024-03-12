3f1a0556e022ee9ec7fa19646c271ed830a9c57e
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-12 18:31:06 +0900

    test: install cypress for some testing

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,24 @@
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/login');
+  });
+
+  it('successfully logs in', () => {
+    // Fill in the login form
+    cy.get('input[type="email"]').type('user@example.com'); // user's email
+    cy.get('input[type="password"]').type('password123'); // user's password
+
+    cy.get('form').submit();
+  });
+  it(' invalid login', () => {
+    // Fill in the login form with invalid credentials
+    cy.get('input[type="email"]').type('wronguser@example.com');
+    cy.get('input[type="password"]').type('wrongpassword');
+
+    // Submit the form
+    cy.get('form').submit();
+
+    // Check that the application did not redirect
+    cy.url().should('include', '/login'); // Make sure we're still on the login page
+  });
+});
