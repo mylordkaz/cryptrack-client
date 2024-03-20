5f18b044f1efc70f5ca78e5e780f61abc01603d6
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-20 11:58:31 +0900

    feat: change rendering of the h1

--- src/components/OverviewCard.tsx
+++ src/components/OverviewCard.tsx
@@ -2,13 +2,18 @@ import './OverviewCard.css';
 
 interface OverviewCardProps {
   totalValue: number;
+  cryptoName?: string | null;
 }
 
-export default function OverviewCard({ totalValue }: OverviewCardProps) {
+export default function OverviewCard({
+  totalValue,
+  cryptoName,
+}: OverviewCardProps) {
   return (
     <>
       <div className="overview">
         <div className="card">
+          <h1>{cryptoName ? cryptoName : 'My portfolio'}</h1>
           <span>${totalValue.toFixed(2)}</span>
         </div>
       </div>
