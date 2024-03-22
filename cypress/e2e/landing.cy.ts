a34e5e200b59ca5dfba61772bbb54a5109529b58
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-22 16:02:13 +0900

    landing page test

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -2,4 +2,21 @@ describe('landing page', () => {
   it('successfully load', () => {
     cy.visit('/');
   });
+
+  it('contains the logo', () => {
+    cy.visit('/');
+    cy.get('img[alt="logo"]').should('be.visible');
+  });
+
+  it('navigates to login page on clicking login', () => {
+    cy.visit('/');
+    cy.contains('Login').click();
+    cy.url().should('include', '/login');
+  });
+
+  it('navigates to register page on clicking Get Started', () => {
+    cy.visit('/');
+    cy.contains('Get started...').click();
+    cy.url().should('include', '/register');
+  });
 });
