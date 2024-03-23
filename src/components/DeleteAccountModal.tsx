727290192fd97baab06e64f9fdf9b3f7e52d1081
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-23 21:46:39 +0900

    feat: create delete modal

---
+++ src/components/DeleteAccountModal.tsx
@@ -0,0 +1,33 @@
+import './deleteModel.css';
+
+interface DeleteTransactionModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const DeleteAccountModal: React.FC<DeleteTransactionModalProps> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+}) => {
+  const handleModalContentClick = (e: { stopPropagation: () => void }) => {
+    e.stopPropagation();
+  };
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <div className="container-modal" onClick={onClose}>
+        <div className="delete-modal" onClick={handleModalContentClick}>
+          <h1>Delete your Account</h1>
+          <p>Are you sure you want to delete your account ?</p>
+          <p>all your data will be lost!</p>
+          <button onClick={onConfirm}>Delete</button>
+          <button onClick={onClose}>Cancel</button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default DeleteAccountModal;
