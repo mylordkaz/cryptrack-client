07d51087b0304a3dfb57c048bcc7ade6d0f83e29
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-06-10 00:17:48 +0900

    create interceptor to attach token to request

---
+++ src/service/api/axios.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const api = axios.create({
+  baseURL: 'https://cryptrack-server.onrender.com',
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// handle token refresh
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const { data } = await api.post('/auth/refresh-token');
+        Cookies.set('accessToken', data.accessToken, {
+          sameSite: 'none',
+          secure: true,
+        });
+        Cookies.set('refreshToken', data.refreshToken, {
+          sameSite: 'none',
+          secure: true,
+        });
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+      } catch (err) {
+        console.error('Token refresh failed', err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
